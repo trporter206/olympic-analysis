6f9951c911043f219726fd15136574e03c4798db
trporter <torri.porter@yahoo.com>
2018-11-17 20:03:01 -0800

    ratio of men to women

--- main.py
+++ main.py
@@ -19,15 +19,39 @@ def overview(data):
     print('non numeric columns')
     print data.describe(include=[np.object])
 
-#fill NA heights
-female_data = olympic_data[olympic_data.Sex == "F"]
-female_height = female_data.Height.dropna().mean().round(0)
-male_data = olympic_data[olympic_data.Sex == "M"]
-male_height = male_data.Height.dropna().mean().round(0)
+# #fill NaN values
+def trait_fill(col, sex):
+    val = olympic_data[olympic_data.Sex == sex][col].dropna().mean().round(0)
 
-olympic_data['Height'] = olympic_data.apply(
-lambda row: female_height if np.isnan(row['Height']) and row['Sex'] is 'F' else row['Height'], axis=1
-)
-olympic_data['Height'] = olympic_data.apply(
-lambda row: male_height if np.isnan(row['Height']) and row['Sex'] is 'M' else row['Height'], axis=1
-)
+    olympic_data[col] = olympic_data.apply(
+    lambda row: val if np.isnan(row[col]) and row['Sex'] is sex else row[col], axis=1
+    )
+
+trait_fill('Height', 'M')
+trait_fill('Height', 'F')
+trait_fill('Weight', 'M')
+trait_fill('Weight', 'F')
+trait_fill('Age', 'M')
+trait_fill('Age', 'F')
+
+#change sex vals to 1 and 0
+for dataset in [olympic_data]:
+    dataset['Sex'] = dataset['Sex'].map({'F': 0, 'M': 1}).astype(int)
+
+p = olympic_data[['Games', 'Sex']].groupby(['Games'], as_index=False).count()
+p1 = olympic_data[['Games', 'Sex']].groupby(['Games'], as_index=False).sum()
+p1 = p1['Sex'].astype(int).tolist()
+p2 = p['Sex'].astype(int).tolist()
+
+female_count = []
+for i in range(len(p1)):
+    female_count.append(p2[i] - p1[i])
+p['F'] = pd.Series(female_count)
+
+for dataset in [p]:
+    dataset['M/F'] = dataset['Sex'] / dataset['F']
+
+# p.plot(x='Games', y=['Sex', 'F'])
+p.plot(x='Games', y='M/F')
+
+plt.show()
